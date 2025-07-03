3042930abf66370f4f6878f4e5348b6ae72e26a2
petr.ourednik <5140-petr.ourednik@users.noreply.gitlab.tuwien.ac.at>
2025-07-03 14:19:34 +0200

    created physcila_constants.py

---
+++ physical_constants.py
@@ -0,0 +1,13 @@
+class PhysicalConstants:
+    """
+    A class to hold various physical constants.
+    """
+
+    def __init__(self):
+        self.h = 6.62607015e-34  # Planck constant (J·s)
+        self.hbar = 1.054571817e-34  # Reduced Planck constant (J·s)
+        self.c_0 = 299792458       # Speed of light (m/s)
+        self.k_B = 1.380649e-23    # Boltzmann constant (J/K)
+        self.e_c = 1.602176634e-19 # Elementary charge (C)
+        self.epsilon_0 = 8.854187817e-12  # Vacuum permittivity (F/m)
+        self.mu_0 = 1.256637061e-6  # Vacuum permeability (H/m)
