fbddfeedc3edb8b3094f59cd4b9a5e429cc7ace2
petr.ourednik <5140-petr.ourednik@users.noreply.gitlab.tuwien.ac.at>
2025-07-05 22:16:33 +0200

    Remove commented debug statements and unused code in transparency_vectorized function

--- modules/barrier_transparency.py
+++ modules/barrier_transparency.py
@@ -15,18 +15,9 @@ def transparency_vectorized(energy_vec, model):
     v3 = model.reg_3.potential_energy
     width = model.barrier.width
 
-    # print(f"Effective masses: m1={m1}, m2={m2}, m3={m3}")
-    # print(f"Potential energies: v1={v1/consts.e_c}, v2={v2/consts.e_c}, v3={v3/consts.e_c}")
-    # print(f"Barrier width: {width}")
-    # print(energy_vec / consts.e_c)
-
     k1 = calc_k(m1, v1, energy_vec)
     k2 = calc_k(m2, v2, energy_vec)
     k3 = calc_k(m3, v3, energy_vec)
-
-    # exp_k2_width = np.exp(1j * k2 * width)
-    # exp_minus_k2_width = np.exp(-1j * k2 * width)
-    # exp_minus_k1_width = np.exp(-1j * k1 * width)
 
     numerator = 2 * k1 *k2*m2*m3
     denominator = k2*m2*(k3*m1 + k1*m3) * np.cos(k2*width) - 1j*(k1*k3*m2**2 + k2**2*m1*m3) * np.sin(k2*width)
@@ -35,15 +26,12 @@ def transparency_vectorized(energy_vec, model):
         t_amp = numerator / denominator
         transparency = (k3 * m1) / (k1 * m3) * np.abs(t_amp)**2
 
-        # # Set transparency to zero where k1 is zero (or very close to zero)
-        # tol = 1e-14
-        # mask = np.abs(k2) < tol
-        # transparency[mask] = (
-        #     (k3 * m1) / (k1 * m3) *
-        #     np.abs(
-        #         (k1 * m2**2 * exp_minus_k1_width) /
-        #         (k1*m2*m3 + k3*m2*m1 + 1j*k1*m2*k3*m2*width)
-        #     )**2)[mask]
+    # Without effective mass dependence
+    # numerator = 4 * k1 * k2
+    # denominator = (k1 + k2)**2 * np.exp(-1j*k2*width) - (k1- k2)**2 * np.exp(1j*k2*width)
+    # with np.errstate(divide='ignore', invalid='ignore'):
+    #     t_amp = numerator / denominator
+    #     transparency = np.abs(t_amp)**2
 
     # Ensure transparency is real-valued
     transparency = np.real(transparency)
